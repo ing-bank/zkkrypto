14a7f4e1b9f7294629533c0c9e3ff7f5105ab76e
alexeykoren <alexey.koren@gmail.com>
2020-09-21 19:39:31 +0200

    1.0.8 - Poseidon hash

--- src/main/kotlin/com/ing/dlt/zkkrypto/ecc/poseidon/PoseidonHash.kt
+++ src/main/kotlin/com/ing/dlt/zkkrypto/ecc/poseidon/PoseidonHash.kt
@@ -1,11 +1,11 @@
 package com.ing.dlt.zkkrypto.ecc.poseidon
 
+import com.ing.dlt.zkkrypto.ecc.curves.BabyJubjub
 import com.ing.dlt.zkkrypto.ecc.poseidon.Constants.Companion.defaultRoundConstants
 import java.math.BigInteger
-import kotlin.math.min
 
 data class PoseidonHash(
-    val r: BigInteger,
+    val r: BigInteger = BabyJubjub.R,
     val constants: Constants = defaultRoundConstants()
 ) {
 
@@ -14,23 +14,7 @@ data class PoseidonHash(
      */
     val hashLength = r.bitLength() / 8 + if(r.bitLength() / 8 != 0) 1 else 0
 
- //   fun hash(msg: ByteArray): ByteArray = hash(bytesToField(msg))
-
-    private fun sbox(m: BigInteger): BigInteger {
-        return m.modPow(BigInteger.valueOf(5), r)
-    }
-
-    fun mix(state: List<BigInteger>, matrix: List<List<BigInteger>>): List<BigInteger> {
-
-        val newState: MutableList<BigInteger> = MutableList(state.size) { BigInteger.ZERO }
-
-        for (i in state.indices) {
-            for (j in state.indices) {
-                newState[i] = newState[i] + matrix[j][i] * state[j]
-            }
-        }
-        return newState
-    }
+ //   TODO fun hash(msg: ByteArray): ByteArray = hash(bytesToField(msg))
 
     fun hash(msg: List<BigInteger>): BigInteger  {
 
@@ -45,7 +29,7 @@ data class PoseidonHash(
 
         val lastRound = constants.numRoundsF + nRoundsP - 1
 
-        for (round in 0..constants.numRoundsF + nRoundsP) {
+        for (round in 0 until constants.numRoundsF + nRoundsP) {
 
             // Add Round Key
             for (i in state.indices) {
@@ -72,17 +56,19 @@ data class PoseidonHash(
         return state[0]
     }
 
-    private fun bytesToField(msg: ByteArray): List<BigInteger> {
+    private fun sbox(m: BigInteger): BigInteger {
+        return m.modPow(BigInteger.valueOf(5), r)
+    }
 
-        // 31 seems to hardcoded and probably should depend on R but I use it like this for compatibility reason
-        val n = 31
-        val ints = mutableListOf<BigInteger>()
+    private fun mix(state: List<BigInteger>, matrix: List<List<BigInteger>>): List<BigInteger> {
 
-        for (i in msg.indices step n) {
-            // We revert array here because bytes are supposed to be little-endian unlike BigInteger
-            val int = BigInteger(1, msg.sliceArray(i until min(i+n, msg.size)).reversedArray())
-            ints.add(int)
+        val newState: MutableList<BigInteger> = MutableList(state.size) { BigInteger.ZERO }
+
+        for (i in state.indices) {
+            for (j in state.indices) {
+                newState[i] = newState[i] + matrix[j][i] * state[j]
+            }
         }
-        return ints
+        return newState
     }
 }
--- src/test/kotlin/com/ing/dlt/zkkrypto/ecc/mimc/Mimc7HashTest.kt
+++ src/test/kotlin/com/ing/dlt/zkkrypto/ecc/mimc/Mimc7HashTest.kt
@@ -44,7 +44,7 @@ class Mimc7HashTest {
         val b78 = BigInteger("78", 10)
         val b41 = BigInteger("41", 10)
 
-        val mimc7 = Mimc7Hash(BigInteger("21888242871839275222246405745257275088548364400416034343698204186575808495617", 10))
+        val mimc7 = Mimc7Hash()
         
         val ints1 = listOf(b12)
         val h1 = mimc7.hash(ints1)
@@ -80,7 +80,7 @@ class Mimc7HashTest {
 
         val msg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-        val mimc7 = Mimc7Hash(BigInteger("21888242871839275222246405745257275088548364400416034343698204186575808495617", 10))
+        val mimc7 = Mimc7Hash()
 
         val h = mimc7.hash(msg.toByteArray())
         assertEquals(
